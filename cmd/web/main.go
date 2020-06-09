988d34243486e6756df7456209e61ff48b951d33
Mario Jackson <mario@jackson.software>
2020-06-09 13:12:17 +0200

    snippet repository: insert

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,8 +3,9 @@ package main
 import (
     "database/sql"
     "flag"
-	"log"
-	"net/http"
+    "jackson.software/snippetbox/pkg/models/mysql"
+    "log"
+    "net/http"
     "os"
 
     _ "github.com/go-sql-driver/mysql"
@@ -12,37 +13,38 @@ import (
 
 type application struct {
     errorLog *log.Logger
-    infoLog *log.Logger
+    infoLog  *log.Logger
+    snippets *mysql.SnippetRepository
 }
 
 func main() {
-	addr := flag.String("addr", ":4000", "HTTP network address")
-	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
-	flag.Parse()
+    addr := flag.String("addr", ":4000", "HTTP network address")
+    dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
+    flag.Parse()
 
-	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile)
+    infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+    errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile)
 
-	db, err := openDB(*dsn)
-	if err != nil {
-	    errorLog.Fatal(err)
+    db, err := openDB(*dsn)
+    if err != nil {
+        errorLog.Fatal(err)
     }
     defer db.Close()
 
-	app := &application{
-	    errorLog: errorLog,
-	    infoLog: infoLog,
+    app := &application{
+        errorLog: errorLog,
+        infoLog:  infoLog,
     }
 
-	srv := &http.Server{
-		Addr: *addr,
-		Handler: app.routes(),
-		ErrorLog: errorLog,
-	}
+    srv := &http.Server{
+        Addr:     *addr,
+        Handler:  app.routes(),
+        ErrorLog: errorLog,
+    }
 
-	infoLog.Printf("Starting server on %s", *addr)
-	err = srv.ListenAndServe()
-	errorLog.Fatal(err)
+    infoLog.Printf("Starting server on %s", *addr)
+    err = srv.ListenAndServe()
+    errorLog.Fatal(err)
 }
 
 func openDB(dsn string) (*sql.DB, error) {
--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -5,18 +5,31 @@ import (
     "jackson.software/snippetbox/pkg/models"
 )
 
-type SnippetModel struct {
-	DB *sql.DB
+type SnippetRepository struct {
+    DB *sql.DB
 }
 
-func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
-    return 0, nil
+func (m *SnippetRepository) Insert(title, content, expires string) (int, error) {
+    stmt := `INSERT INTO snippets (title, content, created, expires)
+    VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+    result, err := m.DB.Exec(stmt, title, content, expires)
+    if err != nil {
+        return 0, err
+    }
+
+    id, err := result.LastInsertId()
+    if err != nil {
+        return 0, err
+    }
+
+    return int(id), nil
 }
 
-func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
+func (m *SnippetRepository) Get(id int) (*models.Snippet, error) {
     return nil, nil
 }
 
-func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
+func (m *SnippetRepository) Latest() ([]*models.Snippet, error) {
     return nil, nil
 }
