acd429b8249268b89d79440dba4b59468eca7c85
Mario Jackson <mario@jackson.software>
2020-06-09 21:19:08 +0200

    Added current year as default to template data

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,40 +1,41 @@
 package main
 
 import (
-    "html/template"
-    "jackson.software/snippetbox/pkg/models"
-    "path/filepath"
+	"html/template"
+	"jackson.software/snippetbox/pkg/models"
+	"path/filepath"
 )
 
 type templateData struct {
-	Snippet *models.Snippet
-	Snippets []*models.Snippet
+	CurrentYear int
+	Snippet     *models.Snippet
+	Snippets    []*models.Snippet
 }
 
 // NewTemplateCache creates a cache of templates indexed by their page name,
 // coming from all *.page.tmpl files found in the given directory, by reading
 // all template files into a map.
 func newTemplateCache(dir string) (map[string]*template.Template, error) {
-    // Initialize map to act like a cache
-    cache := map[string]*template.Template{}
+	// Initialize map to act like a cache
+	cache := map[string]*template.Template{}
 
-    pages, err := filepath.Glob(filepath.Join(dir, "*.page.tmpl"))
-    if err != nil {
-        return nil, err
-    }
+	pages, err := filepath.Glob(filepath.Join(dir, "*.page.tmpl"))
+	if err != nil {
+		return nil, err
+	}
 
-    for _, page := range pages {
-        name := filepath.Base(page)
+	for _, page := range pages {
+		name := filepath.Base(page)
 
-        ts, err := getTemplateForPage(page, dir)
-        if err != nil {
-            return nil, err
-        }
+		ts, err := getTemplateForPage(page, dir)
+		if err != nil {
+			return nil, err
+		}
 
-        cache[name] = ts
-    }
+		cache[name] = ts
+	}
 
-    return cache, nil
+	return cache, nil
 }
 
 // GetTemplateForPage returns a template for the given page eg. home.page.tmpl,
@@ -42,20 +43,20 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 //
 // GetTemplateForPage returns an error when a file could not be parsed.
 func getTemplateForPage(page, dir string) (*template.Template, error) {
-    ts, err := template.ParseFiles(page)
-    if err != nil {
-        return nil, err
-    }
+	ts, err := template.ParseFiles(page)
+	if err != nil {
+		return nil, err
+	}
 
-    ts, err = ts.ParseGlob(filepath.Join(dir, "*.layout.tmpl"))
-    if err != nil {
-        return nil, err
-    }
+	ts, err = ts.ParseGlob(filepath.Join(dir, "*.layout.tmpl"))
+	if err != nil {
+		return nil, err
+	}
 
-    ts, err = ts.ParseGlob(filepath.Join(dir, "*.partial.tmpl"))
-    if err != nil {
-        return nil, err
-    }
+	ts, err = ts.ParseGlob(filepath.Join(dir, "*.partial.tmpl"))
+	if err != nil {
+		return nil, err
+	}
 
-    return ts, nil
+	return ts, nil
 }
