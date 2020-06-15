6284bc4755be48a5f37601c32f0ee4587ea7f86a
Mario Jackson <mario@jackson.software>
2020-06-15 08:06:50 +0200

    added documentation for handlers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,135 +1,140 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"net/http"
-	"strconv"
+    "errors"
+    "fmt"
+    "net/http"
+    "strconv"
 
-	"github.com/go-zoo/bone"
-	"jackson.software/snippetbox/pkg/forms"
-	"jackson.software/snippetbox/pkg/models"
+    "github.com/go-zoo/bone"
+    "jackson.software/snippetbox/pkg/forms"
+    "jackson.software/snippetbox/pkg/models"
 )
 
 // Home handler shows the home page with the latest snippets.
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	s, err := app.snippets.Latest()
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
+    s, err := app.snippets.Latest()
+    if err != nil {
+        app.serverError(w, err)
+        return
+    }
 
-	app.render(w, r, "home.page.tmpl", &templateData{Snippets: s})
+    app.render(w, r, "home.page.tmpl", &templateData{Snippets: s})
 }
 
 // ShowSnippet handler shows a specific snippet.
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(bone.GetValue(r, "id"))
-	if err != nil || id < 1 {
-		app.notFound(w)
-		return
-	}
+    id, err := strconv.Atoi(bone.GetValue(r, "id"))
+    if err != nil || id < 1 {
+        app.notFound(w)
+        return
+    }
 
-	s, err := app.snippets.Get(id)
-	if err != nil {
-		if errors.Is(err, models.ErrNoRecord) {
-			app.notFound(w)
-		} else {
-			app.serverError(w, err)
-		}
-		return
-	}
+    s, err := app.snippets.Get(id)
+    if err != nil {
+        if errors.Is(err, models.ErrNoRecord) {
+            app.notFound(w)
+        } else {
+            app.serverError(w, err)
+        }
+        return
+    }
 
-	app.render(w, r, "show.page.tmpl", &templateData{Snippet: s})
+    app.render(w, r, "show.page.tmpl", &templateData{Snippet: s})
 }
 
 // ShowSnippetForm handler shows a form with fields to create a new snippet.
 func (app *application) showSnippetForm(w http.ResponseWriter, r *http.Request) {
-	app.render(w, r, "create.page.tmpl", &templateData{
-		Form: forms.New(nil),
-	})
+    app.render(w, r, "create.page.tmpl", &templateData{
+        Form: forms.New(nil),
+    })
 }
 
 // CreateSnippet handler creates a new snippet.
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
-	if err != nil {
-		app.clientError(w, http.StatusBadRequest)
-		return
-	}
+    err := r.ParseForm()
+    if err != nil {
+        app.clientError(w, http.StatusBadRequest)
+        return
+    }
 
-	form := forms.New(r.PostForm)
-	form.Required("title", "content", "expires")
-	form.MaxLength("title", 100)
-	form.PermittedValues("expires", "365", "7", "1")
+    form := forms.New(r.PostForm)
+    form.Required("title", "content", "expires")
+    form.MaxLength("title", 100)
+    form.PermittedValues("expires", "365", "7", "1")
 
-	if !form.Valid() {
-		app.render(w, r, "create.page.tmpl", &templateData{Form: form})
-		return
-	}
+    if !form.Valid() {
+        app.render(w, r, "create.page.tmpl", &templateData{Form: form})
+        return
+    }
 
-	id, err := app.snippets.Insert(form.Get("title"), form.Get("content"), form.Get("expires"))
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
+    id, err := app.snippets.Insert(form.Get("title"), form.Get("content"), form.Get("expires"))
+    if err != nil {
+        app.serverError(w, err)
+        return
+    }
 
-	app.session.Put(r, "flash", "Your Snippet was successfully created")
+    app.session.Put(r, "flash", "Your Snippet was successfully created")
 
-	http.Redirect(w, r, fmt.Sprintf("/snippets/%d", id), http.StatusSeeOther)
+    http.Redirect(w, r, fmt.Sprintf("/snippets/%d", id), http.StatusSeeOther)
 }
 
 func (app *application) signupUserForm(w http.ResponseWriter, r *http.Request) {
-	app.render(w, r, "signup.page.tmpl", &templateData{
-		Form: forms.New(nil),
-	})
+    app.render(w, r, "signup.page.tmpl", &templateData{
+        Form: forms.New(nil),
+    })
 }
 
+// SignupUser signs the user up if the given data if the given data is valid,
+// by creating a new account.
 func (app *application) signupUser(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
-	if err != nil {
-		app.clientError(w, http.StatusBadRequest)
-		return
-	}
+    err := r.ParseForm()
+    if err != nil {
+        app.clientError(w, http.StatusBadRequest)
+        return
+    }
 
-	form := forms.New(r.PostForm)
-	form.Required("name", "email", "password")
-	form.MaxLength("name", 255)
-	form.MaxLength("email", 255)
-	form.MatchesPattern("email", forms.EmailRX)
-	form.MinLength("password", 10)
+    form := forms.New(r.PostForm)
+    form.Required("name", "email", "password")
+    form.MaxLength("name", 255)
+    form.MaxLength("email", 255)
+    form.MatchesPattern("email", forms.EmailRX)
+    form.MinLength("password", 10)
 
-	if !form.Valid() {
-		app.render(w, r, "signup.page.tmpl", &templateData{Form: form})
-		return
-	}
+    if !form.Valid() {
+        app.render(w, r, "signup.page.tmpl", &templateData{Form: form})
+        return
+    }
 
-	err = app.users.Insert(form.Get("name"), form.Get("email"), form.Get("password"))
-	if err != nil {
-		if errors.Is(err, models.ErrDuplicateEmail) {
-			form.Errors.Add("email", "Address is already in use")
-			app.render(w, r, "signup.page.tmpl", &templateData{Form: form})
-		} else {
-			app.serverError(w, err)
-		}
-		return
-	}
+    err = app.users.Insert(form.Get("name"), form.Get("email"), form.Get("password"))
+    if err != nil {
+        if errors.Is(err, models.ErrDuplicateEmail) {
+            form.Errors.Add("email", "Address is already in use")
+            app.render(w, r, "signup.page.tmpl", &templateData{Form: form})
+        } else {
+            app.serverError(w, err)
+        }
+        return
+    }
 
-	app.session.Put(r, "flash", "You signup was successful. Please log in")
-	http.Redirect(w, r, "/users/login", http.StatusSeeOther)
+    app.session.Put(r, "flash", "You signup was successful. Please log in")
+    http.Redirect(w, r, "/users/login", http.StatusSeeOther)
 }
 
+// LoginUserForm renders the login page for the user to login.
 func (app *application) loginUserForm(w http.ResponseWriter, r *http.Request) {
-	app.render(w, r, "login.page.tmpl", &templateData{
-		Form: forms.New(nil),
-	})
+    app.render(w, r, "login.page.tmpl", &templateData{
+        Form: forms.New(nil),
+    })
 }
 
+// LoginUser logs the user in by validating the given email and password.
+// If the credentials are invalid, an error will be returned.
 func (app *application) loginUser(w http.ResponseWriter, r *http.Request) {
     err := r.ParseForm()
-	if err != nil {
-	    app.clientError(w, http.StatusBadRequest)
-	    return
+    if err != nil {
+        app.clientError(w, http.StatusBadRequest)
+        return
     }
 
     form := forms.New(r.PostForm)
@@ -148,9 +153,10 @@ func (app *application) loginUser(w http.ResponseWriter, r *http.Request) {
     http.Redirect(w, r, "/snippets/create", http.StatusSeeOther)
 }
 
+// LogoutUser logs the user out by deleting the authentication cookie.
 func (app *application) logoutUser(w http.ResponseWriter, r *http.Request) {
     app.session.Remove(r, "authenticatedUserID")
     app.session.Put(r, "flash", "You've been logged out successfully!")
-    
+
     http.Redirect(w, r, "/", http.StatusSeeOther)
 }
--- pkg/models/mysql/users.go
+++ pkg/models/mysql/users.go
@@ -1,47 +1,47 @@
 package mysql
 
 import (
-	"database/sql"
-	"errors"
-	"strings"
+    "database/sql"
+    "errors"
+    "strings"
 
-	"github.com/go-sql-driver/mysql"
-	"golang.org/x/crypto/bcrypt"
-	"jackson.software/snippetbox/pkg/models"
+    "github.com/go-sql-driver/mysql"
+    "golang.org/x/crypto/bcrypt"
+    "jackson.software/snippetbox/pkg/models"
 )
 
 type UserRepository struct {
-	DB *sql.DB
+    DB *sql.DB
 }
 
 // Insert inserts a new user into the database. If there is already a user
 // by the give data, an error will be returned.
 func (m *UserRepository) Insert(name, email, password string) error {
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
-	if err != nil {
-		return err
-	}
+    hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+    if err != nil {
+        return err
+    }
 
-	var duplicateEntryCode uint16 = 1062
+    var duplicateEntryCode uint16 = 1062
 
-	stmt := `INSERT INTO users (name, email, hashed_password, created)
+    stmt := `INSERT INTO users (name, email, hashed_password, created)
 	VALUES (?, ?, ?, UTC_TIMESTAMP())`
 
-	_, err = m.DB.Exec(stmt, name, email, string(hashedPassword))
-	if err != nil {
-		var mySQLError *mysql.MySQLError
-		if errors.As(err, &mySQLError) {
-			if mySQLError.Number == duplicateEntryCode && strings.Contains(mySQLError.Message, "users_uc_email") {
-				return models.ErrDuplicateEmail
-			}
-		}
-		return err
-	}
+    _, err = m.DB.Exec(stmt, name, email, string(hashedPassword))
+    if err != nil {
+        var mySQLError *mysql.MySQLError
+        if errors.As(err, &mySQLError) {
+            if mySQLError.Number == duplicateEntryCode && strings.Contains(mySQLError.Message, "users_uc_email") {
+                return models.ErrDuplicateEmail
+            }
+        }
+        return err
+    }
 
-	return nil
+    return nil
 }
 
-// Authenticate authenticates a user. If the authentication was not successfull,
+// Authenticate authenticates a user. If the authentication was not successful,
 // an error will be returned.
 func (m *UserRepository) Authenticate(email, password string) (int, error) {
     var id int
@@ -74,5 +74,5 @@ func (m *UserRepository) Authenticate(email, password string) (int, error) {
 // Get gets a user found by the given ID. If no user could be found by
 // the given ID, an error will be returned.
 func (m *UserRepository) Get(id int) (*models.User, error) {
-	return nil, nil
+    return nil, nil
 }
